8f4d513937ade39e187740e3ec98d940aee7f0e0
Abdoulaye Sy <doulsy2021@gmail.com>
2022-09-09 11:59:39 +0000

    html de la page tirage

--- apitirage/src/app/app.module.ts
+++ apitirage/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TirageComponent } from './tirage/tirage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TirageComponent
   ],
   imports: [
     BrowserModule
